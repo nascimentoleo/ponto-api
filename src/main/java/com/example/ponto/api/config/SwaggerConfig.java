9aab3c96e605644dac382a5793dd51cd661dbb0a
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2019-10-08 21:16:01 -0300

    Adicionado swagger UI

---
+++ src/main/java/com/example/ponto/api/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.ponto.api.config;
+
+import com.example.ponto.api.utils.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@Profile("dev")
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Autowired private UserDetailsService userDetailsService;
+  @Autowired private JwtTokenUtil jwtTokenUtil;
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.example.ponto.api.controllers"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("Ponto Inteligente API")
+        .description("Documentação da API de acesso aos endpoints do Ponto Inteligente.")
+        .version("1.0")
+        .build();
+  }
+}
